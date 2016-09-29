242edf5d4245188253a9301bbe613ef53ef28950
jtran93 <johnathan_tran93@yahoo.com>
2016-09-29 15:05:28 -0500

    Update AssetValue.java

--- AssetValue.java
+++ AssetValue.java
@@ -1,49 +1,15 @@
 package assetValue;
 
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class AssetValueTest
+public class AssetValue
 {
-    AssetValue assetValue;
-
-    @Before
-    public void setUp()
+    //Venkat: Is the stockPrice in cents or whole dollars?
+    public int computeAssetValue(int numberOfShares, int stockPrice)
     {
-        assetValue = new AssetValue();
-    }
+        if(numberOfShares < 0 || stockPrice < 0)
+        {
+            return -1;
+        }
 
-    @Test
-    public void Canary()
-    {
-        assertTrue(true);
+        return numberOfShares * stockPrice;
     }
-
-    @Test
-    public void computeAssetValueCorrectValueIsComputed()
-    {
-        assertEquals(12500000, assetValue.computeAssetValue(5000, 2500));
-    }
-//Venkat: The above test was for computeAssetValue. The next test should continue with
-//computeAssetValue. Should not jump to something else suddenly. Start with something,
-//stay with that through a few tests: positive, negative, and may be exception test if needed.
-
-    @Test
-    public void computeAssetValueWithNegativeStockPrice()
-    {
-        assertEquals(-1, assetValue.computeAssetValue(2500, -1));
-    }
-
-    @Test
-    public void computeAssetValueWithNegativeNumOfShares()
-    {
-        assertEquals(-1, assetValue.computeAssetValue(-1, 2500));
-    }
-
-    @Test public void computeAssetValueVeryLargeNumber()
-    {
-        assertEquals(-1, assetValue.computeAssetValue(1500000000, 2500));
-    }
-
 }
