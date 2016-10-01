be272376aeadcd20408460337024b0afe2f5533b
jtran93 <johnathan_tran93@yahoo.com>
2016-10-01 01:43:27 -0500

    Update AssetValue.java

--- AssetValue.java
+++ AssetValue.java
@@ -2,14 +2,31 @@ package assetValue;
 
 public class AssetValue
 {
-    //Venkat: Is the stockPrice in cents or whole dollars?
+    private StockPriceFetcher stockPriceFetcher;
+
+    public AssetValue(StockPriceFetcher theStockPriceFetcher)
+    {
+        stockPriceFetcher = theStockPriceFetcher;
+    }
+
+    //Venkat: How are we going to deal with cents?
     public int computeAssetValue(int numberOfShares, int stockPrice)
     {
-        if(numberOfShares < 0 || stockPrice < 0)
+        if(numberOfShares < 0 || stockPrice <= 0)
         {
-            return -1;
+            return 0;
         }
 
         return numberOfShares * stockPrice;
     }
+
+    protected int getStockPrice(String stock) throws Exception
+    {
+        int price = stockPriceFetcher.getPrice(stock);
+        if(price == -1)
+            throw new Exception();
+        else
+            return stockPriceFetcher.getPrice(stock);
+    }
+
 }
