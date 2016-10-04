7cbac26526dfea77bfc80eb96e1c9dc86710a483
bamtonio <bamtonio@gmail.com>
2016-10-04 16:48:34 -0500

    Update AssetValue.java
    added computeNetAssetValue and the map. the map only stores assetValue and netASsetvalue currently.

--- AssetValue.java
+++ AssetValue.java
@@ -1,19 +1,26 @@
 package assetValue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AssetValue
 {
     private StockPriceFetcher stockPriceFetcher;
-
+    protected Map<String, Object> assetValuesMap = new HashMap<String, Object>();
+    private int netAssetValue = 0;
 
     public AssetValue(StockPriceFetcher theStockPriceFetcher)
     {
         stockPriceFetcher = theStockPriceFetcher;
+        assetValuesMap.put("Net asset values", 0);
     }
 
     public int computeAssetValue(int numberOfShares, int stockPriceWithCents)
     {
         if(numberOfShares < 0 || stockPriceWithCents <= 0)
+        {
             return 0;
+        }
         return numberOfShares * stockPriceWithCents;
     }
 
@@ -21,4 +28,17 @@ public class AssetValue
     {
         return stockPriceFetcher.getPrice(stock);
     }
+
+    public Map<String, Object> computeNetAssetValues(String stock) {
+
+        int assetValue = computeAssetValue(2000,getStockPrice(stock));
+        assetValuesMap.put(stock, assetValue);
+
+        netAssetValue += assetValue;
+        assetValuesMap.put("Net asset values", netAssetValue);
+        return assetValuesMap;
+    }
 }
+
+
+
