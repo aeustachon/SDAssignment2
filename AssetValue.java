977d87cdd0c02f2066e94ee879af9c854891954a
jtran93 <johnathan_tran93@yahoo.com>
2016-10-07 15:05:33 -0500

    Update AssetValue.java

--- AssetValue.java
+++ AssetValue.java
@@ -4,53 +4,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AssetValue
-{
-    private final int SIZE = 10;
+public class AssetValue {
     private StockPriceFetcher stockPriceFetcher;
-    protected Map<String, Object> assetValuesMap = new HashMap<String, Object>();
-    private int netAssetValue = 0;
-    ArrayList<String> listOfStocksWithInvalidTickers = new ArrayList<String>();
-    ArrayList<String> listOfStocksWithNetworkErrors = new ArrayList<String>();
 
-    public AssetValue(StockPriceFetcher theStockPriceFetcher)
-    {
+    public AssetValue(StockPriceFetcher theStockPriceFetcher) {
         stockPriceFetcher = theStockPriceFetcher;
     }
 
-    public int computeAssetValue(int numberOfShares, int stockPriceWithCents)
-    {
-        if(numberOfShares < 0 || stockPriceWithCents <= 0)
-        {
+    public int computeAssetValue(int numberOfShares, int stockPriceWithCents) {
+        if (numberOfShares < 0 || stockPriceWithCents <= 0) {
             return 0;
         }
         return numberOfShares * stockPriceWithCents;
     }
 
-    protected int getStockPrice(String stock)
-    {
+    protected int getStockPrice(String stock) {
         return stockPriceFetcher.getPrice(stock);
     }
 
-    public Map<String, Object> computeNetAssetValues(String stock) {
-        if(getStockPrice(stock) == 0)
-        {
-            listOfStocksWithInvalidTickers.add(stock);
-            assetValuesMap.put("Invalid symbol", listOfStocksWithInvalidTickers);
-        }
-        else if (getStockPrice(stock) == -1)
-        {
-            listOfStocksWithNetworkErrors.add(stock);
-            assetValuesMap.put("Network error", listOfStocksWithNetworkErrors);
-        }
-        int assetValue = computeAssetValue(2000,getStockPrice(stock));
-        assetValuesMap.put(stock, assetValue);
+    public Map<String, Object> computeNetAssetValues(Map<String, Integer> stocksAndShares) {
 
-        netAssetValue += assetValue;
-        assetValuesMap.put("Net asset values", netAssetValue);
+        int totalAssetValue = 0;
+
+        Map<String, Object> assetValuesMap = new HashMap<String, Object>();
+        ArrayList<String> listOfStocksWithInvalidTickers = new ArrayList<String>();
+        ArrayList<String> listOfStocksWithNetworkErrors = new ArrayList<String>();
+
+        for (String stock : stocksAndShares.keySet()) {
+            try {
+                getStockPrice(stock);
+                int assetValue = computeAssetValue(stocksAndShares.get(stock), getStockPrice(stock));
+                assetValuesMap.put(stock, assetValue);
+                totalAssetValue += assetValue;
+            }catch (IllegalArgumentException e) {
+                listOfStocksWithInvalidTickers.add(stock);
+            }
+            catch(IllegalStateException e) {
+                listOfStocksWithNetworkErrors.add(stock);
+            }
+        }
+        assetValuesMap.put("INVALID SYMBOLS", listOfStocksWithInvalidTickers);
+        assetValuesMap.put("NETWORK ERRORS", listOfStocksWithNetworkErrors);
+        assetValuesMap.put("TOTAL", totalAssetValue);
         return assetValuesMap;
     }
 }
-
-
-
